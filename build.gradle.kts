e4493c1ccfe7c66ae509706f64fb4dc510200aba
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2023-10-30 21:09:01 +0100

    chors(dep): spelling mistake

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    api(kotlin("refelect"))
+    api(kotlin("reflect"))
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation(kotlin("test"))
