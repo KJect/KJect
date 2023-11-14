c372b4ddc103ed937d5a08f48943c5d27b7c71f6
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2023-11-14 15:29:27 +0100

    fix: remove unused method

--- src/main/kotlin/me/kject/internal/dependency/trace/DependencyTraceImpl.kt
+++ src/main/kotlin/me/kject/internal/dependency/trace/DependencyTraceImpl.kt
@@ -7,22 +7,6 @@ import kotlin.reflect.KClass
 
 class DependencyTraceImpl(override val elements: List<DependencyTraceElement>) : DependencyTrace {
 
-    override fun toString() = buildString {
-        var start = true
-
-        for (i in elements.indices) {
-            val element = elements[i]
-
-            if (start) start = false
-            else append(" -> ")
-
-            append(element)
-            if (i != elements.lastIndex) appendLine()
-        }
-
-        if (start) append("No dependencies")
-    }
-
     override fun toString(duplicate: KClass<*>)= buildString {
         var start = true
 
